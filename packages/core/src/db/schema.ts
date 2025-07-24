2e1f25a4f647543482a8e5bfe50940d49b0cc74b
Michael <michaeljohneduave@gmail.com>
2025-07-24 11:54:03 -0700

    Move drizzle schema to core package

--- packages/core/src/db/schema.ts
+++ packages/core/src/db/schema.ts
@@ -1,7 +1,8 @@
-import type { formCompleterSchema } from "@auto-apply/core/src/schema";
+import type { InferSelectModel } from "drizzle-orm";
 import * as t from "drizzle-orm/sqlite-core";
 import { sqliteTable } from "drizzle-orm/sqlite-core";
 import type z from "zod";
+import type { formCompleterSchema } from "../schema";
 import type { SessionCost } from "../types";
 
 const sessionStatus = ["started", "done"] as const;
@@ -11,6 +12,7 @@ export const sessions = sqliteTable("sessions", {
 		autoIncrement: true,
 	}),
 	userId: t.text("user_id").notNull(),
+	title: t.text("title").notNull(),
 	companyName: t.text("company_name").notNull(),
 	url: t.text("url").notNull(),
 	status: t.text({ enum: sessionStatus }).default("started"),
@@ -37,4 +39,8 @@ export const users = sqliteTable("users", {
 	userId: t.text("user_id").notNull(),
 	baseResumeMd: t.text("base_resume_md").notNull(),
 	personalInfoMd: t.text("personal_info_md").notNull(),
+	baseResumeLatex: t.text("base_resume_latex").notNull(),
 });
+
+export type Sessions = InferSelectModel<typeof sessions>;
+export type Users = InferSelectModel<typeof users>;
