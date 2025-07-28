92ec065767ace75281b5ce3f7b15a3a55e303621
Michael <michaeljohneduave@gmail.com>
2025-07-28 15:43:49 -0700

    Use pnpm

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	plugins: [viteReact(), tailwindcss()],
 	server: {
+		port: 5175,
 		proxy: {
 			"/api": {
 				target: "http://localhost:5000",
